29a1378a049b6e4bdabb82f1dbea57c362ee0ecf
Ebrahim573 <101477670+Ebrahim573@users.noreply.github.com>
2022-08-03 10:24:40 +0530

    new

--- src/app/modules/booking/booking.component.ts
+++ src/app/modules/booking/booking.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatTableDataSource } from '@angular/material/table';
+import { NgForm } from '@angular/forms';
+import * as _ from 'lodash';
+import { Book } from 'src/app/model/book';
+import { BookService } from 'src/app/service/book.service';
 
 @Component({
   selector: 'app-booking',
@@ -7,9 +13,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BookingComponent implements OnInit {
 
-  constructor() { }
+
+  @ViewChild('bookForm',{static:false})
+  bookForm! :NgForm;
+
+  bookData!:Book;
+  dataSource =new MatTableDataSource();
+  displayedColumns: string[]=['id','hallname','capacity','personincharge','hallbookingprice','actions'];
+  @ViewChild(MatPaginator, {static: true})
+  paginator!: MatPaginator;
+  isEditMode =false;
+
+
+
+
+
+  constructor(private BookService: BookService) { }
+
+
 
   ngOnInit(): void {
+
+
+this.dataSource.paginator=this.paginator;
+this.getAllBook()
+
   }
 
+  getAllBook(){
+    this.BookService.getList().subscribe((response: any)=> {
+      this.dataSource.data =response;
+    });
+    }
+editItem(element:any){
+  this.bookData= _.cloneDeep(element);
+  this.isEditMode=true;
 }
+cancelEdit(){
+  this.isEditMode=false;
+  this.bookForm.resetForm();
+}
+
+deleteItem(id:string){
+  this.BookService.deleteItem(id).subscribe.data.filter((o:any)=>{
+return o.id !==id ? o :false;
+  });
+  console.log(this.dataSource.data);
+
+
+}
+
+
+
+  }
+
